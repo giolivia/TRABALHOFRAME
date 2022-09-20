8f4c0d87838d50f08059039201a6ac9d524ce5f9
giolivia <99481242+giolivia@users.noreply.github.com>
2022-09-20 11:56:08 -0300

    Update cabecalho.jsx

--- src/components/cabecalho/cabecalho.jsx
+++ src/components/cabecalho/cabecalho.jsx
@@ -5,17 +5,17 @@ export const Cabecalho = () =>{
         <header>
                 <div className='logonome'>
                     <div>
-                        <img src='logobranco.png' className='log'></img>
+                        <img src='logobranco.png' className='log' alt='imagem da logo'></img>
                         <h1>BocaWeb</h1>
                     </div>
 
                     <div className='historia'>
-                        <img src='icone-info.png' className='icone'></img>
+                        <img src='icone-info.png' className='icone' alt='imagem do icone de informação'></img>
                         História
                     </div>
                 </div>
                 <div className='loginandimg'>
-                    <img src='icone-login.png' class='icone'></img>
+                    <img src='icone-login.png' class='icone' alt='imagem do icone de login'></img>
                     
                     Login
                 </div>
